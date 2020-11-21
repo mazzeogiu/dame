a23f6652717a435fb093115a6c09f9ea2f2d71de
Giuseppe M <mazzeogiu@gmail.com>
2020-11-21 17:22:39 +0100

    add table and movement logic

--- js/dama.js
+++ js/dama.js
@@ -1,23 +1,21 @@
-//  ERRORE DI LOGICA segnala due movimenti possibili quando invece si può solo mangiare. Errore al primo ciclo di for della funzione checkMove
-//  DA SVILUPPARE
-//  sviluppare il mangia pedina
-//  mangia pedina multiplo da sviluppare
-//  pedina in dama
-//  sistema di calcolo del vincitore
+//  LOGIC ERROR signals two possible movements when you can only eat. Error on first for loop of checkMove function
+//  TO DEVELOP
+//  develop the pawn eater
+//  eat multiple pawn to develop
+//  pawn in checkers
+//  winner calculation system
 //
 
 $(function() {
     let numberOfLine = 8;
-    let numberOfRow = 8;
+    let numberOfColumn = 8;
     let numberPieces = 24
     let config = [["blackTable","blackPawn.png", "black"], ["whiteTable","whitePawn.png", "white"]];
     let currentClass = 0;
     let indexPawn = 0;
     let nextPlayer = 1;
     let removePawn = "";
-    // 0=posizione vuota
-    // 1= pedine nere
-    // 2= pedine bianche
+    
     let letter = ["A", "B", "C", "D", "E", "F", "G", "H"];
     let positionOfPawn =[[9, 9, 9, 9, 9, 9, 9, 9, 9, 9], 
                         [9, 1, 0, 1, 0, 1, 0, 1, 0, 9], 
@@ -32,35 +30,35 @@ $(function() {
     // BEGIN table costruction
     for(let i = 0; i < numberOfLine; i++){
         currentLine =  $('<tr>').attr("id", i);
-        // il risultato del div assumerà il valore -1 oppure 1
-        //questo ci permetterà di creare l'alternanza 0 e 1 alla linea 38
         currentLineClass = i % 2 == 0 ? -1 : 1;  
         currentClass = currentLineClass;
-        for(let y = 0; y < numberOfRow; y++){
+
+        for(let y = 0; y < numberOfColumn; y++){
             currentRow = $('<td>').attr("id", currentLine.attr("id") + "-" + y)
             index = currentClass > 0 ? currentClass : 0;
             displayClass= config[index][0];
-            currentRow.addClass(displayClass);
-            //Posiziona le pedine
+            currentRow.addClass(displayClass); 
+            //Place the pieces
             if (index == 0){
-                // Lascia le 2 righe centrali vuote
+                // Leave the 2 middle lines blank
                 if(i < 3 || i > 4){
                     indexPawn = numberPieces > 12 ? 0 : 1;
-                    // inserisco la classe "move" per indicare il giocatore che ha diritto a mouvere
+                    // I insert the class "move" to indicate the player who has the right to move
                     currentRow.html('<img class="'+config[indexPawn][2]+'" id="' + i + '_' + y + '" src="image/'+config[indexPawn][1]+'" alt="'+config[indexPawn][1]+'"></img>');
                     numberPieces -= 1;
                 }  
             }
             currentLine.append(currentRow);
-            // crea l'alternanza di 0 e 1 
             currentClass = currentClass * -1;
         }
-        $("table").append(currentLine);
+        // Print next player to move
+        $("#table").append(currentLine);
     }
+    $("#player").text(config[nextPlayer][2]);
     // END table costruction
     // BEGIN hover enter control
     $(".white, .black").on("mouseenter", function(){
-        // l'if esegue lo script per il giocatore che ha il diritto alla mossa
+        // the if executes the script for the player who is entitled to move
         if($(this).attr("class") == config[nextPlayer][2]){
             currentPositionPawn = $(this).attr("id");
             color = $(this).attr("class");
@@ -85,17 +83,17 @@ $(function() {
         $("td").removeAttr("ondrop ondragover style");
         let idParent = $(this).parent().attr("id");
         currentPositionPawn = $(this).attr("id");
-        //controllo se il drag and drop è stato eseguito
+        //check if the drag and drop has been performed
         if(parseInt(currentPositionPawn) !== parseInt(idParent)){
-            // Rimuovo pedina mangiata
-            // removePawn != "" ? $("#" + removePawn + " img").remove() : removePawn = "";
-            // Aggiorno l'id della pedina
+            // Update the pawn id
             $(this).attr("id", idParent[0] + "_" + idParent[2]);
             nextPlayer ? nextPlayer = 0 : nextPlayer = 1;
+            // Stampa a video il prossimo giocatore che muove
+            $("#player").text(config[nextPlayer][2]);
         }
     });
     // END hover leave control
-    // BEGIN calcolo mosse possibili e cattura pedina avversaria
+    // BEGIN calculation of possible moves and capture of opponent's pawn
     function checkMove(newPosition, currentPosition, player){
         let eatPawn = false;
         let row = 0;
@@ -104,20 +102,17 @@ $(function() {
         let colorPawn = "";
         for(i = 0; i < newPosition.length; i++) {
             colorPawn = $("#" + newPosition[i] + " img").attr("class");
-            // Verifico se le caselle in cui si può muovere non sia vuota e se l'eventuale pedina sia dell'avversario 
+            // Check if the squares in which you can move are not empty and if the possible piece belongs to the opponent
             if(colorPawn != player && typeof(colorPawn) != "undefined"){
-                // Calcolo posizione di destinazione per mangiare la pedina avversaria 
+                // Calculation of target position to eat the opponent's piece
                 row = Number(newPosition[i][0]) - Number(currentPosition[0]);
                 column = Number(newPosition[i][2]) - Number(currentPosition[2]);
                 tempPosition = String(parseInt(newPosition[i][0]) + row) + "-" + String(parseInt(newPosition[i][2]) + column);
-                // Verifico se la casella dopo aver mangiato è libera per validare la possibile mossa
+                // I check if the space after eating is free to validate the possible move
                 colorPawn = $("#" + tempPosition + " img").attr("class")
                 typeof(colorPawn) !="undefined" ? eatPawn = false : eatPawn = true;
-                // se eatPawn è true memorizzo la posizione della pedina da mangiare
-                // eatPawn ? removePawn = newPosition[i] : removePawn = "";
-                // Se eatPawn è falso annullo la possibile mossa
                 eatPawn ? newPosition[i] = tempPosition : newPosition[i] = "--";
-            // Inibisce la mossa su una pedina dello stesso colore
+            // Inhibits the move on a pawn of the same color
             }else if(colorPawn == player || eatPawn){
                 newPosition[i] = "--";
             } 
@@ -125,11 +120,11 @@ $(function() {
         console.log(removePawn)
     return newPosition;
     }
-    // END calcolo mosse possibili e cattura pedina avversaria
+    // END calculation of possible moves and capture of opponent's pawn
 
 // -------------
 });
-// INIZIO drag and drop
+// BEGIN drag and drop
 function allowDrop(ev) {
     ev.preventDefault();
   }
@@ -141,4 +136,4 @@ function drop(ev) {
     let data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
-//FINE drag and drop
+//END drag and drop
